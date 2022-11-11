b1aa898aed8a05bf9bfb9a6f0809b65cb130fa99
timothy hoover <bnhilliard>
2022-11-10 20:07:37 -0800

    post not working

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -8,30 +8,40 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     Actions.get()
-    .then(result =>
-        res.send(result))
+    .then(result => {
+        res.status(200).json(result)})
     .catch(err => res.send(err))
-})
+});
 
-router.get('/:id', (req, res) => {
-
-})
+router.get('/:id', (req, res, next) => {
+    Actions.get(req.params.id)
+        .then(result => {
+           if (!result) { 
+            res.status(404).json('Not Found') 
+        } else { res.status(200).json(result) 
+        }}).catch(err => res.send(err))
+});
 
 router.post('/', (req, res) => {
-
+    Actions.insert({description: req.body.description})
+    .then(result => 
+        res.send(result))
+    .catch(err => 
+        res.send({message: err.message, stack: err.stack}))
 })
+
 
 router.put('/:id', (req, res) => {
 
-})
+});
 
 router.delete('/:id', (req, res) => {
 
-})
+});
 
 router.get('/:id/actions', (req, res) => {
 
-})
+});
 
 
 
--- api/server.js
+++ api/server.js
@@ -9,12 +9,6 @@ server.use('/api/actions', actionsRouter)
 
 server.use('/api/projects', projectsRouter)
 
-
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
-
 server.get('/', (req, res) => {
     res.send(`Let's do this!`)
 })
